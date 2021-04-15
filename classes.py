30aa2fd67cc379f7ada229f5ac0a171de9545d55
ll220 <lucylin220@gmail.com>
2021-04-14 20:12:00 -0700

    Added bank and state classes and functions

---
+++ classes.py
@@ -0,0 +1,93 @@
+######################################################################################################################################
+# Includes classes for banks and states and functions that allows the user to determine valid actions based off of state configuration 
+# Also includes functions that determine whether or not state configurations are equal or not
+######################################################################################################################################
+
+import threading, queue
+import copy
+
+####################################################################
+# Actions are encoded as: 
+#     1: Send one chicken across
+#     2: Send two chickens across
+#     3: Send one wolf across
+#     4: Send one wolf and one chicken across
+#     5: Send two wolves across
+####################################################################
+
+# Represents each bank that exists in each state. Contains number of boats, chickens and wolves 
+class bank: 
+    def __init__(self, c, w, b):
+        self.chickens = c
+        self.wolves = w
+        self.boat = b
+
+# Represents each state, contains a list that contains previous states as previous actions encoded with integers
+class gameState(bank): 
+    def __init__(self, c_left, w_left, boat_left, c_right, w_right, prev = []): 
+        self.leftBank = bank(c_left, w_left, boat_left)
+        self.rightBank = bank(c_right, w_right, not boat_left)
+        self.prevStates = copy.deepcopy(prev)
+
+    # Returns a list of five Booleans. Each of them represents actions: [1, 2, 3, 4, 5]. True indicates if the next valid and a successor can be generated via that action
+    def checkValidSuccessors (self): 
+        validActions = [False, False, False, False, False]
+
+        if (self.leftBank.boat):   # Check possible actions moving right, assuming the banks are in a valid state
+            # Check if the left bank has chickens to send
+            if (self.leftBank.chickens > 0):
+                # You can have more wolves than chickens on the left if there are no chickens, else check if sending one chicken does not violate conditions 
+                if (self.rightBank.wolves <= self.rightBank.chickens + 1 and (self.leftBank.chickens == 1 or self.leftBank.wolves <= self.leftBank.chickens - 1)):
+                    validActions[0] = True 
+                # If two chickens can be sent, if conditions are not violated on the left and does not violate conditions for right
+                if (self.leftBank.chickens >= 2 and self.rightBank.wolves <= self.rightBank.chickens + 2 and (self.leftBank.chickens - 2 == 0 or self.leftBank.wolves <= self.leftBank.chickens - 2)):
+                    validActions[1] = True 
+                # If there are wolves on the left side and if wolves <= chickens on the right side (there could be some wolves but no chickens)
+                if (self.leftBank.wolves > 0 and self.rightBank.wolves <= self.rightBank.chickens): 
+                    validActions[3] = True
+
+            # Check if the left bank has wolves to send
+            if (self.leftBank.wolves > 0):
+                # Can send any number of wolves over if there are no chickens on the right side, else, check if adding wolf to the right side doesn't violate conditions
+                if (self.rightBank.chickens == 0 or self.rightBank.wolves + 1 <= self.rightBank.chickens): 
+                    validActions[2] = True
+                # If there are two wolves to send and again, checking if no chickens or if adding 2 wolves will violate right side
+                if (self.leftBank.wolves >= 2 and (self.rightBank.chickens == 0 or (self.rightBank.wolves + 2 <= self.rightBank.chickens))): 
+                    validActions[4] = True
+
+        else: 
+            if (self.rightBank.chickens > 0):
+                # Check if can send one chicken over to left 
+                if (self.leftBank.wolves <= self.leftBank.chickens + 1 and (self.rightBank.chickens == 1 or self.rightBank.wolves <= self.rightBank.chickens - 1)):
+                    validActions[0] = True 
+                # Check if two chickens can be sent to the left
+                if (self.rightBank.chickens >= 2 and self.leftBank.wolves <= self.leftBank.chickens + 2 and (self.rightBank.chickens == 2 or self.rightBank.wolves <= self.rightBank.chickens - 2)):
+                    validActions[1] = True 
+                # Check if a wolf and chicken can be sent to the left
+                if (self.rightBank.wolves > 0 and self.leftBank.wolves <= self.leftBank.chickens): 
+                    validActions[3] = True
+
+            # Check if the right bank has wolves to send
+            if (self.rightBank.wolves > 0):
+                # Check if can send one wolf over
+                if (self.leftBank.chickens == 0 or self.leftBank.wolves + 1 <= self.leftBank.chickens): 
+                    validActions[2] = True
+                # Check if two wolves can be sent over left
+                if (self.rightBank.wolves >= 2 and (self.leftBank.chickens == 0 or (self.leftBank.wolves + 2 <= self.leftBank.chickens))): 
+                    validActions[4] = True
+        return validActions
+
+# Checks if two banks have equal values
+def checkBanksEqual(firstBank, secondBank):
+    if (firstBank.chickens == secondBank.chickens and firstBank.wolves == secondBank.wolves and firstBank.boat == secondBank.boat):
+        return True
+    else:
+        return False
+
+# Checks if two states have equal configurations (disregards the list of previous actions)
+def checkStatesEqual(firstState, secondState):
+    if (checkBanksEqual(firstState.leftBank, secondState.leftBank) and checkBanksEqual(firstState.rightBank, secondState.rightBank)):
+        return True
+    else:
+        return False
+
